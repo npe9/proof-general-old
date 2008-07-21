fd7206ba6fdef29c9f5293a957c9d3eabc2530ed
D. A. <d.a@example.com>
2008-07-21 17:56:02 +0000

    Change format of proof-toolbar-entries-default; simplify toolbar code.

--- generic/pg-custom.el
+++ generic/pg-custom.el
@@ -56,32 +56,25 @@ support depends on whether your proof assistant supports it."
   :group 'proof-user-options)
 
 (defconst proof-toolbar-entries-default
-  `((state	"Display Proof State" "Display the current proof state" t
-				      proof-showproof-command)
-    (context	"Display Context"     "Display the current context" t
-				      proof-context-command)
-;; PG 3.7: disable goal & qed, they're not so useful (& save-command never enabled).
-;;     (goal	"Start a New Proof"   "Start a new proof" t
-;;  				      proof-goal-command)
-    (retract	"Retract Buffer"      "Retract (undo) whole buffer" t)
-    (undo	"Undo Step"           "Undo the previous proof command" t)
-    (delete	"Delete Step"         nil t)
-    (next	"Next Step"           "Process the next proof command" t)
-    (use	"Use Buffer"  	      "Process whole buffer" t)
-    (goto	"Goto Point"	      "Process or undo to the cursor position" t)
-;;     (qed	"Finish Proof"        "Close/save proved theorem" t
-;; 				      proof-save-command)
-    (lockedend  "Goto Locked End"     nil t)
-    (find	"Find Theorems"	      "Find theorems" t proof-find-theorems-command)
-    (command    "Issue Command"	      "Issue a non-scripting command" t)
-    (interrupt  "Interrupt Prover"    "Interrupt the proof assistant" t)
-    (restart	"Restart Scripting"   "Restart scripting (clear all locked regions)" t)
-    (visibility "Toggle Visibility"   nil t)
-; PG 3.6: remove Info item from toolbar; it's not very useful and under PA->Help anyway
-;    (info	nil		      "Show online proof assistant information" t
-;				       proof-info-command)
-; PG 3.7: use Info icon for info
-    (info	nil		      "Proof General manual" t))
+  `((state "Display Proof State" "Display the current proof state" t
+	   proof-showproof-command)
+    (context "Display Context" "Display the current context" t
+	     proof-context-command)
+    (goal      "Start a New Proof" "Start a new proof" t nil)
+    (retract   "Retract Buffer"     "Retract (undo) whole buffer" t)
+    (undo      "Undo Step"          "Undo the previous proof command" t)
+    (delete    "Delete Step"        "Delete the last proof command" t)
+    (next      "Next Step"          "Process the next proof command" t)
+    (use       "Use Buffer"         "Process whole buffer" t)
+    (goto      "Goto Point"         "Process or undo to the cursor position" t)
+    (qed       "Finish Proof"       "Close/save proved theorem" t nil)
+    (lockedend "Goto Locked End"    "Goto end of the last command proceesed" nil t)
+    (find      "Find Theorems"	    "Find theorems" t proof-find-theorems-command)
+    (command   "Issue Command"	    "Issue a non-scripting command" nil)
+    (interrupt "Interrupt Prover"   "Interrupt the proof assistant" t)
+    (restart   "Restart Scripting"  "Restart scripting (clear all locked regions)" t)
+    (visibility "Toggle Visibility" "Show or hide hidden proofs" nil t)
+    (help	nil		    "Proof General manual" t t))
 "Example value for proof-toolbar-entries.  Also used to define scripting menu.
 This gives a bare toolbar that works for any prover, providing the
 appropriate configuration variables are set.
@@ -91,18 +84,19 @@ defining functions, images.")
 
 (defpgcustom toolbar-entries proof-toolbar-entries-default
   "List of entries for Proof General toolbar and Scripting menu.
-Format of each entry is (TOKEN MENUNAME TOOLTIP DYNAMIC-ENABLER-P ENABLE).
+Format of each entry is (TOKEN MENUNAME TOOLTIP TOOLBAR-P [VISIBLE-P]).
 
 For each TOKEN, we expect an icon with base filename TOKEN,
 a function proof-toolbar-<TOKEN>, and (optionally) a dynamic enabler
 proof-toolbar-<TOKEN>-enable-p.
 
-If ENABLEP is absent, item is enabled; if ENABLEP is present, item
-is only added to menubar and toolbar if ENABLEP is non-null.
+If VISIBLE-P is absent, or evaluates to non-nil, the item will
+appear on the toolbar or menu.  If it evaluates to nil, the item
+is not shown.
 
 If MENUNAME is nil, item will not appear on the scripting menu.
 
-If TOOLTIP is nil, item will not appear on the toolbar.
+If TOOLBAR-P is nil, item will not appear on the toolbar.
 
 The default value is `proof-toolbar-entries-default' which contains
 the standard Proof General buttons.")
--- generic/proof-toolbar.el
+++ generic/proof-toolbar.el
@@ -6,22 +6,12 @@
 ;;
 ;; $Id$
 ;;
-;;
-;; 1. edit-toolbar cannot edit proof toolbar (even in a proof mode)
-;; Need a variable containing  a specifier or similar.
-;; (defvar proof-toolbar-specifier nil
-;;   "Specifier for proof toolbar.")
-;; This doesn't seem worth fixing until XEmacs toolbar implementation
-;; settles a bit.  Enablers don't work too well at the moment.
-
+;;; Commentary:
+;; 
 ;; 2. It's a little bit tricky to add prover-specific items:
 ;; presently it must be done before this file is loaded.
 ;; We could improve on that by generating everything on-thy-fly
 ;; in proof-toolbar-setup.
-
-;; 3. We could consider automatically disabling buttons which are
-;; not configured for the prover, e.g. if proof-info-command is
-;; not set, then the Info button should not be shown.
 ;;
 ;; See `proof-toolbar-entries-default' and `<PA>-toolbar-entries'
 ;; in pg-custom for the default generic toolbar and
@@ -33,8 +23,6 @@
   (require 'proof-utils))
 
 
-;;; Commentary:
-;; 
 
 (require 'span)
 (require 'proof-config)
@@ -53,9 +41,6 @@
 (defun proof-toolbar-enabler (token)
   (intern (concat "proof-toolbar-" (symbol-name token) "-enable-p")))
 
-(defun proof-toolbar-function-with-enabler (token)
-  (intern (concat "proof-toolbar-" (symbol-name token) "-with-enabler-p")))
-
 ;;
 ;; Now the toolbar icons and buttons
 ;; 
@@ -63,64 +48,47 @@
 (defun proof-toolbar-make-icon (tle)
   "Make icon variable and icon list entry from a PA-toolbar-entries entry."
   (let* ((icon     (car tle))
-	 (tooltip  (nth 2 tle))
+	 (toolbarp (nth 3 tle))
 	 (iconname (symbol-name icon))
 	 (iconvar  (proof-toolbar-icon icon)))
-    ;; first declare variable
-    ;;  (eval
-    ;;  `(defvar ,iconvar nil
-    ;;  ,(concat
-    ;;   "Glyph list for " iconname " button in Proof General toolbar.")))
-    ;; FIXME: above doesn't quite work right.  However, we only lose
-    ;; the docstring which is no big deal.
-    ;; now the list entry
-    (if tooltip
-	(list (list iconvar iconname)))))
-  
+    (when toolbarp
+      (set iconvar (concat "epg-" iconname)))))
 
-(defun proof-toolbar-make-toolbar-item (tle)
-  "Make a toolbar button descriptor from a PA-toolbar-entries entry."
-  (let*
-      ((token	      (nth 0 tle))
-       (includep      (or (< (length tle) 5) (eval (nth 4 tle))))
-       (menuname      (and includep (nth 1 tle)))
-       (tooltip       (and includep (nth 2 tle)))
-       (existsenabler (nth 3 tle))
-       (enablep	      (and proof-toolbar-use-button-enablers
-			   (>= emacs-major-version 21)
-			   existsenabler))
-       (enabler	      (proof-toolbar-enabler token))
-       (enableritem   (if enablep (list enabler) t))
-       (buttonfn      (proof-toolbar-function token))
-       (buttonfnwe    (proof-toolbar-function-with-enabler token))
-       (icon	      (proof-toolbar-icon token))
-       (actualfn
-	(if (or enablep (not existsenabler))
-	    buttonfn
-	  ;; Add the enabler onto the function if necessary.
-	  (eval `(defun ,buttonfnwe ()
-		   (interactive)
-		   (if (,enabler)
-		       (call-interactively (quote ,buttonfn))
-		     (message "Button \"%s\" disabled" ,menuname))))
-	  buttonfnwe)))
-    (if tooltip	;; no tooltip means menu-only item
-	  (list (append (list icon actualfn token
-			      :help tooltip)
-			(if enabler (list :enable (list enabler))))))))
-		
-
+(defun proof-toolbar-make-toolbar-items (map tles)
+  "Make toolbar button descriptors from a PA-toolbar-entries entry."
+  ;; Entry format:  (TOKEN MENUNAME TOOLTIP TOOLBAR-P [VISIBLE-P])
+  (dolist (tle tles)
+    (let* ((token     (nth 0 tle))
+	   (includep  (nth 3 tle))
+	   (visiblep  (nth 4 tle))
+	   (icon      (proof-toolbar-icon token))
+	   (buttonfn  (proof-toolbar-function token))
+	   (enabler   (proof-toolbar-enabler token))
+	   (tooltip   (and includep (nth 2 tle)))
+	   (props     (append
+		       (list :help tooltip)
+		       (if (fboundp enabler)
+			   (list :enable (list enabler)))
+		       (if visiblep 
+			   (list :visible visiblep)))))
+      (if includep
+	  (apply 'tool-bar-local-item
+		 (eval icon) buttonfn token map props)))))
 
 ;;
 ;; Code for displaying and refreshing toolbar
 ;;
 
-(defvar proof-toolbar nil
-  "Proof mode toolbar button list.  Set in `proof-toolbar-build'.
-For GNU Emacs, this holds a keymap.")
+(defvar proof-toolbar-map nil
+  "Proof mode toolbar button list.  Set in `proof-toolbar-setup'.")
 
-(deflocal proof-toolbar-itimer nil
-  "itimer for updating the toolbar in the current buffer")
+(defun proof-toolbar-available-p ()
+  (and ;; Check toolbar support...
+   window-system
+   (featurep 'tool-bar)	                ;; GNU Emacs tool-bar library
+   (or (image-type-available-p 'xpm)    ;; and XPM
+       (image-type-available-p 'png)))) ;; or PNG
+
 
 ;;;###autoload
 (defun proof-toolbar-setup ()
@@ -128,65 +96,17 @@ For GNU Emacs, this holds a keymap.")
 If `proof-toolbar-enable' is nil, change the current buffer toolbar
 to the default toolbar."
   (interactive)
-  (if
-   (and ;; Check toolbar support...
-    window-system
-    (or (and (featurep 'tool-bar)	; GNU Emacs tool-bar library
-	     (or (image-type-available-p 'xpm)   ;; and XPM
-		 (image-type-available-p 'png))) ;; or PNG
-	(and (featurep 'toolbar)	; or XEmacs toolbar library
-	     (featurep 'xpm))))		; and XPM support
-
-      ;; Toolbar support is possible.
-      (progn
-	;; Check the toolbar has been built.
-	(or proof-toolbar (proof-toolbar-build))
-
-	;; Now see if user wants toolbar (this can be changed dyamically).
-	(when proof-toolbar-enable
-	    ;; Enable the toolbar in this buffer: make a local tool-bar-map
-	    (set (make-local-variable 'tool-bar-map) proof-toolbar))
-	
-	(when (not proof-toolbar-enable)
-	  (kill-local-variable 'tool-bar-map)) ;; remove local value
-
-	;; Update the display
-	(sit-for 0))))
-
-(defun proof-toolbar-build ()
-  "Build `proof-toolbar'."
-  (let ((icontype ".xpm")	
-	;; List is (VAR IMAGE). Toolbar code expects to find files IMAGE.xpm
-	(proof-toolbar-icon-list
-	 (apply 'append
-		(mapcar 'proof-toolbar-make-icon
-			(proof-ass toolbar-entries))))
-
-	;; A toolbar descriptor evaluated in proof-toolbar-setup.
-	;; Specifically, a list of sexps which evaluate to entries in
-	;; a toolbar descriptor.  The default
-	;; `proof-toolbar-default-button-list' works for any prover.
-	(proof-toolbar-button-list
-	 (apply 'append (mapcar 'proof-toolbar-make-toolbar-item
-				(proof-ass toolbar-entries)))))
-
-    ;; First set the button variables to glyphs (bit long-windedly).
-    (mapcar
-     (lambda (buttons)
-       (set (car buttons)
-	    (concat "epg-" (eval (cadr buttons)))))
-     proof-toolbar-icon-list)
-
-    (setq proof-toolbar (make-sparse-keymap))
-    (add-to-list 'image-load-path proof-images-directory) ; rude?
-    (dolist (but proof-toolbar-button-list)
-      (apply
-	 'tool-bar-local-item
-	 (eval (nth 0 but))		; image filename
-	 (nth 1 but)			; function symbol
-	 (nth 2 but)			; dummy key
-	 proof-toolbar
-	 (nthcdr 3 but)))))		; remaining properties
+  (when (proof-toolbar-available-p)
+    (unless proof-toolbar-map
+      (setq proof-toolbar-map (make-sparse-keymap))
+      (add-to-list 'image-load-path proof-images-directory) ; rude?
+      (mapcar 'proof-toolbar-make-icon (proof-ass toolbar-entries))
+      (proof-toolbar-make-toolbar-items proof-toolbar-map
+					(proof-ass toolbar-entries)))
+    (when proof-toolbar-enable
+      (set (make-local-variable 'tool-bar-map) proof-toolbar-map))
+    (when (not proof-toolbar-enable)
+      (kill-local-variable 'tool-bar-map))))
 
 ;; Action to take after altering proof-toolbar-enable
 (defalias 'proof-toolbar-enable 'proof-toolbar-setup)
@@ -194,57 +114,41 @@ to the default toolbar."
 ;;;###autoload (autoload 'proof-toolbar-toggle "proof-toolbar")
 (proof-deftoggle proof-toolbar-enable proof-toolbar-toggle)
 
-(defvar proof-toolbar-enablers
-   (mapcar (lambda (tle)
-	     (list (proof-toolbar-enabler (car tle))))
-	   (proof-ass toolbar-entries))
-  "List of all toolbar's enablers")
-
-(defvar proof-toolbar-enablers-last-state
-  nil
-  "Last state of the toolbar's enablers")
-
-
-;;
-;; =================================================================
 ;;
 ;;
-;; GENERIC PROOF TOOLBAR BUTTON FUNCTIONS
+;; Proof General Toolbar and Scripting Menu Functions
+;; --------------------------------------------------
 ;;
 ;; Defaults functions are provided below for: up, down, restart
 ;; Code for specific provers may define the symbols below to use
 ;; the other buttons: next, prev, goal, qed (images are provided).
 ;;
 ;;  proof-toolbar-next		   next function
-;;  proof-toolbar-next-enable      enable predicate for next (or t)
+;;  proof-toolbar-next-enable      enable predicate for next
 ;;
-;; etc.
+;; If no -enable function is defined, button is always enabled.
 ;;
 ;; To add support for more buttons or alter the default
 ;; images, <PA>-toolbar-entries should be adjusted.
 ;; See proof-config.el for that.
 ;;
 ;; Note that since the toolbar is displayed for goals and response
-;; buffers too, enablers and command functions must potentially
-;; switch buffer first.
-;;
+;; buffers too, enablers and command functions must potentially switch
+;; buffer first.
 ;;
 
+;; Undo
 
-;;
-;; Undo button
-;;
+(defalias 'proof-toolbar-undo 'proof-undo-last-successful-command)
 
 (defun proof-toolbar-undo-enable-p ()
   (proof-with-script-buffer
    (and (proof-shell-available-p)
 	(> (proof-unprocessed-begin) (point-min)))))
 
-(defalias 'proof-toolbar-undo 'proof-undo-last-successful-command)
+;; Delete
 
-;;
-;; Delete button  (not actually on toolbar)
-;;
+(defalias 'proof-toolbar-delete 'proof-undo-and-delete-last-successful-command)
 
 (defun proof-toolbar-delete-enable-p ()
   (proof-with-script-buffer
@@ -252,88 +156,49 @@ to the default toolbar."
 	(proof-shell-available-p)
 	(> (proof-unprocessed-begin) (point-min)))))
 
-(defalias 'proof-toolbar-delete 'proof-undo-and-delete-last-successful-command)
-
-
-;;
-;; Lockedend button  (not actually on toolbar)
-;;
-
-(defun proof-toolbar-lockedend-enable-p ()
-  t)
+;; Lockedend
 
 (defalias 'proof-toolbar-lockedend 'proof-goto-end-of-locked)
 
+;; Next
 
-
-
-;;
-;; Next button
-;;
+(defalias 'proof-toolbar-next 'proof-assert-next-command-interactive)
 
 (defun proof-toolbar-next-enable-p ()
   (proof-with-script-buffer
    (not (proof-locked-region-full-p))))
 
-(defalias 'proof-toolbar-next 'proof-assert-next-command-interactive)
+;; Goto
 
-
-;;
-;; Goto button
-;;
+(defalias 'proof-toolbar-goto 'proof-goto-point)
 
 (defun proof-toolbar-goto-enable-p ()
   (eq proof-buffer-type 'script))
 
-(defalias 'proof-toolbar-goto 'proof-goto-point)
+;; Retract
 
-
-;;
-;; Retract button
-;;
+(defalias 'proof-toolbar-retract 'proof-retract-buffer)
 
 (defun proof-toolbar-retract-enable-p ()
   (proof-with-script-buffer
    (not (proof-locked-region-empty-p))))
 
-(defalias 'proof-toolbar-retract 'proof-retract-buffer)
+;; Use
 
-
-;;
-;; Use button
-;;
-
-(defalias 'proof-toolbar-use-enable-p 'proof-toolbar-next-enable-p)
 (defalias 'proof-toolbar-use 'proof-process-buffer)
+(defalias 'proof-toolbar-use-enable-p 'proof-toolbar-next-enable-p)
 
-;;
-;; Restart button
-;;
-
-(defun proof-toolbar-restart-enable-p ()
-  ;; Could disable this unless there's something running.
-  ;; But it's handy to clearup extents, etc, I suppose.
-  t)
+;; Restart
 
 (defalias 'proof-toolbar-restart 'proof-shell-restart)
 
-;;
-;; Goal button
-;;
-
-(defun proof-toolbar-goal-enable-p ()
-  ;; Goals are always allowed, provided proof-goal-command is set.
-  ;; Will crank up process if need be.
-  ;; (Actually this should only be available when "no subgoals")
-  proof-goal-command)
-
+;; Goal
 
 (defalias 'proof-toolbar-goal 'proof-issue-goal)
 
+;; QED
 
-;;
-;; QED button
-;;
+(defalias 'proof-toolbar-qed 'proof-issue-save)
 
 (defun proof-toolbar-qed-enable-p ()
   (proof-with-script-buffer
@@ -341,124 +206,85 @@ to the default toolbar."
 	proof-shell-proof-completed
 	(proof-shell-available-p))))
 
-(defalias 'proof-toolbar-qed 'proof-issue-save)
+;; State
 
-;;
-;; State button
-;;
-
-(defun proof-toolbar-state-enable-p ()
-  (proof-shell-available-p))
-  
 (defalias 'proof-toolbar-state 'proof-prf)
+(defalias 'proof-toolbar-state-enable-p 'proof-shell-available-p)
 
-;;
-;; Context button
-;;
+;; Context
 
-(defun proof-toolbar-context-enable-p ()
-  (proof-shell-available-p))
-  
 (defalias 'proof-toolbar-context 'proof-ctxt)
+(defalias 'proof-toolbar-context-enable-p 'proof-shell-available-p)
 
-;;
-;; Info button
-;;
-;; Might as well enable it all the time; convenient trick to
-;; start the proof assistant.
-
-(defun proof-toolbar-info-enable-p ()
-  t)
+;; Info
 
 (defalias 'proof-toolbar-info 'proof-help)
 
-;;
-;; Command button
-;;
-
-(defun proof-toolbar-command-enable-p ()
-  (proof-shell-available-p))
+;; Command
 
 (defalias 'proof-toolbar-command 'proof-minibuffer-cmd)
+(defalias 'proof-toolbar-command-enable-p 'proof-shell-available-p)
 
-;;
-;; Help button
-;;
+;; Help
  
-(defun proof-toolbar-help-enable-p ()
-  t)
-
 (defun proof-toolbar-help ()
   (interactive)
   (info "ProofGeneral"))
 
-;;
-;; Find button
-;;
+;; Find
  
-(defun proof-toolbar-find-enable-p ()
-  (proof-shell-available-p))
-
 (defalias 'proof-toolbar-find 'proof-find-theorems)
+(defalias 'proof-toolbar-find-enable-p 'proof-shell-available-p)
 
-;;
-;; Visibility button (not on toolbar)
-;;
+;; Visibility (not on toolbar)
+
+(defalias 'proof-toolbar-visibility 'pg-toggle-visibility)
 
 (defun proof-toolbar-visibility-enable-p ()
   (span-property-safe (span-at (point) 'type) 'idiom))
 
-(defalias 'proof-toolbar-visibility 'pg-toggle-visibility)
-
-;;
-;; Interrupt button
-;; 
-
-(defun proof-toolbar-interrupt-enable-p ()
-  proof-shell-busy)
+;; Interrupt
 
 (defalias 'proof-toolbar-interrupt 'proof-interrupt-process)
-
+(defun proof-toolbar-interrupt-enable-p () proof-shell-busy)
 
 ;;
-;; =================================================================
-;;
-;; Scripting menu built from toolbar functions
+;; Scripting Menu
 ;;
 
-(defun proof-toolbar-make-menu-item (tle)
-  "Make a menu item from a PA-toolbar-entries entry."
-  (let*
-      ((token	  (car tle))
-       (menuname  (cadr tle))
-       (tooltip   (nth 2 tle))
-       (enablep	  (nth 3 tle))
-       (fnname	  (proof-toolbar-function token))
-       ;; fnval: remove defalias to get keybinding onto menu; 
-       ;; NB: function and alias must both be defined for this
-       ;; to work!!
-       (fnval	  (if (symbolp (symbol-function fnname))
-		      (symbol-function fnname)
-		    fnname)))
-    (if menuname
-	(list
-	 (apply 'vector
-	   (append
-	    (list menuname fnval)
-	    (if enablep
-		(list ':active (list (proof-toolbar-enabler token))))))))))
-
+;; TODO: pass in map argument, don't use easymenu.
 ;;;###autoload
 (defun proof-toolbar-scripting-menu ()
   "Menu made from the Proof General toolbar commands."
-  ;; Prevent evaluation too early here, otherwise this is called
-  ;; during compilation when loading files with expanded easy-menu-define
-  ;; (e.g. pg-response/proof-aux-menu call)
-  (apply 'append
-	 (mapcar 'proof-toolbar-make-menu-item
-		 (proof-ass toolbar-entries))))
+  (let (menu)
+    (dolist (tle (proof-ass toolbar-entries))
+      ;; Entry format:  (TOKEN MENUNAME TOOLTIP TOOLBAR-P VISIBLE-P)
+      (let* ((token	(car tle))
+	     (menuname  (cadr tle))
+	     (tooltip   (nth 2 tle))
+	     (visiblep  (nth 4 tle))
+	     (enabler   (proof-toolbar-enabler token))
+	     (fnname	  (proof-toolbar-function token))
+	     ;; fnval: remove defalias to get keybinding onto menu;
+	     ;; NB: function and alias must both be defined for this
+	     ;; to work!!
+	     (fnval	  (if (symbolp (symbol-function fnname))
+			      (symbol-function fnname)
+			    fnname)))
+	(when (and menuname (eval visiblep))
+	  (setq menu
+		(cons
+		 (vconcat
+		  (vector menuname fnval :help tooltip)
+		  (if (fboundp enabler)
+		      ;; NB: :active not :enable, for easymenu
+		      (vector :active (list (proof-toolbar-enabler token))))
+		  (if visiblep
+		      (vector :visible visiblep)))
+		 menu)))))
+    (reverse menu)))
 
- 
+
 (provide 'proof-toolbar)
 
 ;;; proof-toolbar.el ends here
